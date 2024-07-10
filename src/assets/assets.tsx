7dc568988d2826a52fdb782dc0cd8161265a049f
hxerimione <purejang98@gmail.com>
2024-07-10 20:21:51 +0900

    design: 새로운 이미지 파일 추가

---
+++ src/assets/assets.tsx
@@ -0,0 +1,9 @@
+export const BtnLeft = require("../assets/images/btn_left.png");
+export const ChatUser = require("../assets/images/chat_user.png");
+export const IconBell = require("../assets/images/icon_bell.png");
+export const IconSearch = require("../assets/images/icon_search.png");
+export const IconPlus = require("../assets/images/icon_plus.png");
+export const IconSend = require("../assets/images/icon_send.png");
+
+//test
+export const DummyProfile = require("../assets/images/dummyprofile.png");
