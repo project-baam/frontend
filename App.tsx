ada4b1a70c041882e93e1800684c86580a88ca2e
itsmesunky <itisp_06@naver.com>
2024-07-17 22:38:32 +0900

    feat: SafeAreaView, StatusBar 컴포넌트 추가

--- App.tsx
+++ App.tsx
@@ -1,12 +1,27 @@
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import Router from "./src/router/Router";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { StatusBar, useColorScheme } from "react-native";
+import { Colors } from "react-native/Libraries/NewAppScreen";
 
 function App(): React.JSX.Element {
+  const isDarkMode = useColorScheme() === "dark";
+
+  const backgroundStyle = {
+    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter
+  };
+
   return (
-    <NavigationContainer>
-      <Router />
-    </NavigationContainer>
+    <SafeAreaView style={backgroundStyle}>
+      <StatusBar
+        barStyle={isDarkMode ? "light-content" : "dark-content"}
+        backgroundColor={backgroundStyle.backgroundColor}
+      />
+      <NavigationContainer>
+        <Router />
+      </NavigationContainer>
+    </SafeAreaView>
   );
 }
 export default App;
