ef19374a7e79ca9ffbfae6851d9cda5915045799
aowjarkwk <justgam12@gmail.com>
2024-07-18 11:02:01 +0900

    feat: Chip 컴포넌트 구현

--- src/router/Router.tsx
+++ src/router/Router.tsx
@@ -1,14 +1,16 @@
 // Router.js
-import React, { useEffect } from "react";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import React from "react";
 
 import ChatScreen from "../pages/ChatScreen";
+import TestScreen from "../pages/TestScreen";
 
 const Stack = createNativeStackNavigator();
 
 const Router = () => {
   return (
     <Stack.Navigator screenOptions={{ headerShown: false }}>
+      <Stack.Screen name="TestScreen" component={TestScreen} />
       <Stack.Screen name="ChatScreen" component={ChatScreen} />
     </Stack.Navigator>
   );
