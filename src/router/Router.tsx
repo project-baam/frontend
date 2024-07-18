43773b45164126f8256100b0185d517a23439b44
aowjarkwk <justgam12@gmail.com>
2024-07-18 11:08:16 +0900

    테스트용 스크린 삭제

--- src/router/Router.tsx
+++ src/router/Router.tsx
@@ -3,14 +3,12 @@ import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import React from "react";
 
 import ChatScreen from "../pages/ChatScreen";
-import TestScreen from "../pages/TestScreen";
 
 const Stack = createNativeStackNavigator();
 
 const Router = () => {
   return (
     <Stack.Navigator screenOptions={{ headerShown: false }}>
-      <Stack.Screen name="TestScreen" component={TestScreen} />
       <Stack.Screen name="ChatScreen" component={ChatScreen} />
     </Stack.Navigator>
   );
