b04a427f631a8be7045646ea62a5f2c2af8e1eb2
itsmesunky <itisp_06@naver.com>
2024-08-20 12:23:35 +0900

    Refactor: IconButton Pressable 컴포넌트 추가

--- src/components/common/button/styles.ts
+++ src/components/common/button/styles.ts
@@ -1,11 +1,16 @@
 import styled from "@emotion/native";
 import { Theme } from "../../../styles/theme";
 
-export const StyledView = styled.View<{ type: "Rec" | "Ball"; number: number; padding: number }>`
+export const StyledView = styled.View<{
+  type: "Rec" | "Ball";
+  number: number;
+  padding: number;
+  bgColor: string;
+}>`
   width: ${({ number }) => `${number}px`};
   height: ${({ number }) => `${number}px`};
   border-radius: ${({ type }) => (type === "Rec" ? "0" : "3000px")};
-  background-color: ${Theme.colors.Gray900};
+  background-color: ${(bgColor) => `${bgColor}`};
   padding: ${({ padding }) => `${padding}px`};
   justify-content: center;
   align-items: center;
