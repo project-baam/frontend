511a32f42ca5967942596e7b1c4275a38a2241de
itsmesunky <itisp_06@naver.com>
2024-08-26 01:43:33 +0900

    asset: Image 추가

--- src/assets/assets.tsx
+++ src/assets/assets.tsx
@@ -12,5 +12,6 @@ export const CalendarIcon = require("../assets/images/icon_calendar.png");
 export const SettingIcon = require("../assets/images/icon_setting.png");
 export const AppleImg = require("../assets/images/apple.png");
 export const KakaoImg = require("../assets/images/kakao.png");
+export const LogoImg = require("../assets/images/baam_logo.png");
 //test
 export const DummyProfile = require("../assets/images/dummyprofile.png");
