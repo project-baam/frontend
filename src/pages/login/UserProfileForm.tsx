0a30f59d9d02d29bee18637ff08c74e6f7a7317c
itsmesunky <itisp_06@naver.com>
2024-08-28 17:26:19 +0900

    feat: 유저 프로필 이미지 설정 페이지 구현

--- src/pages/login/UserProfileForm.tsx
+++ src/pages/login/UserProfileForm.tsx
@@ -1,5 +1,56 @@
-import { View } from "react-native";
+import styled from "@emotion/native";
+import { Theme } from "../../styles/theme";
+import { CardBackgroundImg, PencilImg } from "../../assets/assets";
+import { useState } from "react";
+import ImagePickerComponent from "../../components/common/ImagePicker";
 
 export default function UserProfileForm() {
-  return <View></View>;
+  const [selectedBackgroundImage, setSelectedBackgroudImage] = useState<string | null>();
+
+  function takeImageHandler(imageUri: string) {
+    setSelectedBackgroudImage(imageUri);
+  }
+
+  let backgroundImagePreview = <BackgroundImage source={CardBackgroundImg} />;
+
+  if (selectedBackgroundImage) {
+    backgroundImagePreview = <BackgroundImage source={{ uri: selectedBackgroundImage }} />;
+  }
+
+  return (
+    <RootContainer>
+      <CardContainer>
+        {backgroundImagePreview}
+        <ImagePickerComponent onTakeImage={takeImageHandler}>
+          <EditIcon source={PencilImg} />
+        </ImagePickerComponent>
+      </CardContainer>
+    </RootContainer>
+  );
 }
+
+const RootContainer = styled.View`
+  flex: 1;
+  background-color: ${Theme.colors.White};
+  padding-top: 148px;
+  padding-bottom: 24px;
+  padding-horizontal: 16px;
+  align-items: center;
+`;
+
+const CardContainer = styled.View`
+  position: relative;
+  width: 328px;
+  height: 200px;
+  border-radius: 16px;
+`;
+
+const EditIcon = styled.Image`
+  width: 32px;
+  height: 32px;
+`;
+
+const BackgroundImage = styled.Image`
+  width: 328px;
+  height: 200px;
+`;
