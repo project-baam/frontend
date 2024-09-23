be88d2fee5cead81b45176248a938c3a48998818
itsmesunky <itisp_06@naver.com>
2024-09-23 18:03:55 +0900

    fix: import 경로 수정

--- src/components/calendar/AgendaItem.tsx
+++ src/components/calendar/AgendaItem.tsx
@@ -4,7 +4,7 @@ import React, { useCallback } from "react";
 import { Pressable, Text, View } from "react-native";
 import { Theme } from "../../styles/theme";
 import { Agenda } from "../../types/agenda";
-import { CalendarAddScreenNavigationProp } from "../../types/navigation";
+import { CalendarAddScreenNavigationProp } from "../../navigations/CalendarStackNavigation";
 
 interface ItemProps {
   item: Agenda;
