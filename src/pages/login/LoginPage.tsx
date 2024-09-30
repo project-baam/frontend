5dcf67af4e3067f9b6fdc5e47051da730a783a3c
itsmesunky <itisp_06@naver.com>
2024-09-30 23:12:33 +0900

    ✨ feat: 로그인 플로우 로직 및 UI 수정

--- src/pages/login/LoginPage.tsx
+++ src/pages/login/LoginPage.tsx
@@ -4,10 +4,81 @@ import { AnimatedLogoImage, AppleImg, KakaoImg } from "../../assets/assets";
 import { StackScreenProps } from "@react-navigation/stack";
 import { SignUpStackParamList } from "../../navigations/SignUpStackNavigation";
 import LottieView from "lottie-react-native";
+import appleAuth from "@invertase/react-native-apple-authentication";
+import useAuthStore from "@/store/UserAuthStore";
+import axios from "axios";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { useState } from "react";
+import LoadingOverlay from "@/components/common/ui/LoadingOverlay";
 
 type LoginPageProps = StackScreenProps<SignUpStackParamList, "LoginPage">;
 
 export default function LoginPage({ navigation }: LoginPageProps) {
+  const { setRefreshToken, setToken, setIsAuthenticated } = useAuthStore();
+
+  const [isTryingLogin, setIsTryingLogin] = useState(false);
+
+  const fetchToken = async () => {
+    const storedToken = await AsyncStorage.getItem("refreshToken");
+
+    if (storedToken) {
+      axios
+        .post(
+          "https://b-site.site/authentication/refresh-tokens",
+          {
+            refreshToken: storedToken
+          },
+          {
+            headers: {
+              accept: "application/json",
+              "Content-Type": "application/json"
+            }
+          }
+        )
+        .then((response) => {
+          const accessToken = response.data.accessToken;
+          const refreshToken = response.data.refreshToken;
+          AsyncStorage.setItem("refreshToken", refreshToken);
+          AsyncStorage.setItem("accessToken", accessToken);
+
+          setRefreshToken(refreshToken);
+          setToken(accessToken);
+          setIsAuthenticated(true);
+        })
+        .catch((error) => {
+          console.error(error);
+        });
+    } else {
+      navigation.navigate("KakaoLoginPage");
+    }
+
+    setTimeout(() => {
+      setIsTryingLogin(false);
+    }, 2500);
+  };
+
+  if (isTryingLogin) {
+    return <LoadingOverlay />;
+  }
+
+  async function handleSignInApple() {
+    console.log("click");
+    // performs login request
+    const appleAuthRequestResponse = await appleAuth.performRequest({
+      requestedOperation: appleAuth.Operation.LOGIN,
+      // Note: it appears putting FULL_NAME first is important, see issue #293
+      requestedScopes: [appleAuth.Scope.FULL_NAME, appleAuth.Scope.EMAIL]
+    });
+    // get current authentication state for user
+    // /!\ This method must be tested on a real device. On the iOS simulator it always throws an error.
+    const credentialState = await appleAuth.getCredentialStateForUser(appleAuthRequestResponse.user);
+    console.log(credentialState);
+
+    // use credentialState response to ensure the user is authenticated
+    if (credentialState === appleAuth.State.AUTHORIZED) {
+    }
+  }
+
   return (
     <S.RootContainer>
       <S.ImageContainer>
@@ -21,7 +92,7 @@ export default function LoginPage({ navigation }: LoginPageProps) {
       <S.ButtonContainer>
         <Pressable
           onPress={() => {
-            navigation.navigate("KakaoLoginPage");
+            fetchToken();
           }}
         >
           <S.PrimaryButtonContainer>
@@ -29,10 +100,12 @@ export default function LoginPage({ navigation }: LoginPageProps) {
             <S.PrimaryButtonText>카카오로 로그인</S.PrimaryButtonText>
           </S.PrimaryButtonContainer>
         </Pressable>
-        <S.SecondaryButtonContainer>
-          <Image source={AppleImg} style={{ width: 20, height: 24 }} />
-          <S.SecondaryButtonText>애플로 로그인</S.SecondaryButtonText>
-        </S.SecondaryButtonContainer>
+        <Pressable onPress={handleSignInApple}>
+          <S.SecondaryButtonContainer>
+            <Image source={AppleImg} style={{ width: 20, height: 24 }} />
+            <S.SecondaryButtonText>애플로 로그인</S.SecondaryButtonText>
+          </S.SecondaryButtonContainer>
+        </Pressable>
       </S.ButtonContainer>
     </S.RootContainer>
   );
